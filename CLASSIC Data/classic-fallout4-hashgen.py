58d7c5c6d01d1b0a9f91d300b03a584fc803a4ea
Andrew Nelson <evildarkarchon@gmail.com>
2025-01-11 16:25:35 -0800

    Fix path issue with the open statement.

--- CLASSIC Data/classic-fallout4-hashgen.py
+++ CLASSIC Data/classic-fallout4-hashgen.py
@@ -1,6 +1,6 @@
 ﻿import hashlib
 from pathlib import Path
 
-with Path.open(f"{__file__}/databases/CLASSIC Fallout4.yaml", "rb") as f:
+with Path.open(f"{Path(__file__).parent}/databases/CLASSIC Fallout4.yaml", "rb") as f:
     data = f.read()
     Path("databases/CLASSIC Fallout4.yaml.sha256").write_text(hashlib.sha256(data).hexdigest())
